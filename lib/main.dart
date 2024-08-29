faa68c05a970b6f37a343c2a9f80a5a192c44bb1
Mahmoud4456 <bnyadm2213@gmail.com>
2024-08-29 17:40:55 +0300

    Multiple bloc provider in main & add bloc consumer in bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:hive_test/cubit/add_note_cubit/add_note_cubit.dart';
 import 'package:hive_test/models/note_model.dart';
 import 'package:hive_test/views/home_view.dart';
-
 import 'conestanse.dart';
 
 void main() async{
@@ -19,10 +20,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   return  MaterialApp(
-     home: const HomeView() ,
-     theme: ThemeData(
-       brightness: Brightness.dark
+   return  MultiBlocProvider(
+       providers: [
+      BlocProvider(create: (context) => AddNoteCubit()),
+       ],
+       child: MaterialApp(
+       home: const HomeView() ,
+       theme: ThemeData(
+         brightness: Brightness.dark
+       ),
      ),
    );
   }
--- lib/widgets/custom_bottom_sheet.dart
+++ lib/widgets/custom_bottom_sheet.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_test/cubit/add_note_cubit/add_note_cubit.dart';
+import 'package:hive_test/cubit/add_note_cubit/add_note_state.dart';
 import 'package:hive_test/widgets/note_form_field.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class CustomBottomSheet extends StatelessWidget {
   const CustomBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const AddForm();
+    return  BlocConsumer < AddNoteCubit , AddNoteState >(
+        listener: (BuildContext context, state) {
+          if(state is AddNoteFailure){
+            print("Error ${state.errMessage}");
+          }
+          if (state is AddNoteSuccess){
+            Navigator.pop(context);
+          }
+        },
+        builder: (BuildContext context, state) {
+          return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: const AddForm ()
+          );
+        },
+        );
   }
 }
 
